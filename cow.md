bd18ec5fa7e49da003182f45aaec0ce6fcd65e04
SiZheng <zhangszh7@qq.com>
2026-04-15 15:22:29 +0800

    lab: cow

--- cow.md
+++ cow.md
@@ -1,2 +1,7 @@
 # Lab: Copy-on-Write Fork  
 
+## downside
+- The count array is too big, which almost costs 2MB and most are unused.
+- The Structure of the trap handler is the same as that of the copyout handler,
+so it's wise to define a new function for clear and sharing.
+
--- kernel/vm.c
+++ kernel/vm.c
@@ -375,15 +375,11 @@ copyout(pagetable_t pagetable, uint64 dstva, char *src, uint64 len)
     while(len > 0){
         va0 = PGROUNDDOWN(dstva);
 
-        if(va0 > MAXVA)
+        if(va0 >= MAXVA)
             return -1;
         pte = walk(pagetable, va0, 0);
 
         if(pte == 0)
-            return -1;
-        if((*pte & PTE_V) == 0)
-            return -1;
-        if((*pte & PTE_U) == 0)
             return -1;
 
         // COW PAGE
